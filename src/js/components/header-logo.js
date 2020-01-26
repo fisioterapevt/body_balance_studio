59d5132caca8e1466bf6c74c514b1aa75fbf455a
Tim Bright <fisioterapevt@gmail.com>
2020-01-26 09:39:10 +0600

    add logo bodybalance (svg)

--- src/js/components/header-logo.js
+++ src/js/components/header-logo.js
@@ -3,11 +3,9 @@ import AbstractComponent from './abstract-component';
 const createLogoHeaderTemplate = () => {
   return (
     `<a class="logo" href="https://www.bodybalance.kz">
-        <picture>
-          <source media="(min-width: 1280px)" srcset="assets/img/logo-desktop.png" alt="logo studio BodyBalance" width="500" height="208">
-          <source media="(min-width: 768px)" srcset="assets/img/logo-tablet.png" alt="logo studio BodyBalance" width="300" height="125">
-          <img src="assets/img/logo-mobile.png" alt="logo studio BodyBalance" width="120" height="50">
-        </picture>
+      <svg width="291" height="121" viewBox="0 0 291.11 121">
+        <use xlink:href="assets/img/sprite.svg#body_balance_logo"></use>
+      </svg>
     </a>`
   );
 };
